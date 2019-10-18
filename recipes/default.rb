78e298c0a56ff9d6cdd17cec10833c87c5375aef
muralimohan Badagala <badagalamurali@gmail.com>
2019-10-18 11:22:00 +0000

    First Commit with Simple Cookbook

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+package "apache2" do
+  action :install
+end
+
+service "apache2" do
+  action [:enable, :start]
+end
